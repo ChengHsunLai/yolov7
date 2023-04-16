3046e141424f5621fab28948f5b2d73b46b2b461
ChengHsunLai <kaoru09160916@gmail.com>
2023-04-17 07:43:15 +0800

    Add files via upload
    modify fo YY

--- test.py
+++ test.py
@@ -71,8 +71,10 @@ def test(data,
     model.eval()
     if isinstance(data, str):
         is_coco = data.endswith('coco.yaml')
-        with open(data) as f:
-            data = yaml.load(f, Loader=yaml.SafeLoader)
+        with open(data,'rb') as f:
+            data = yaml.safe_load(f)
+            #data=f.read()
+
     check_dataset(data)  # check
     nc = 1 if single_cls else int(data['nc'])  # number of classes
     iouv = torch.linspace(0.5, 0.95, 10).to(device)  # iou vector for mAP@0.5:0.95
