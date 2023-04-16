7630bf60521c1f741fb7184c948757017289a8de
ChengHsunLai <kaoru09160916@gmail.com>
2023-04-17 07:45:15 +0800

    Add files via upload
    modify fo YY

--- utils/metrics.py
+++ utils/metrics.py
@@ -213,7 +213,8 @@ def plot_mc_curve(px, py, save_dir='mc_curve.png', names=(), xlabel='Confidence'
 
     if 0 < len(names) < 21:  # display per-class legend if < 21 classes
         for i, y in enumerate(py):
-            ax.plot(px, y, linewidth=1, label=f'{names[i]}')  # plot(confidence, metric)
+    #        ax.plot(px, y, linewidth=1, label=f'{names[i]}')  # plot(confidence, metric)
+            ax.plot(px, y)  # plot(confidence, metric)
     else:
         ax.plot(px, py.T, linewidth=1, color='grey')  # plot(confidence, metric)
 
